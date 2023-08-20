75cefe5ebd76ccc5dfbc2dae95cb74276108a24e
alexo8ec <alexo8ec@hotmail.com>
2023-08-20 15:07:32 -0500

    revision login 19/08/2023

--- resources/views/layouts/view_child.blade.php
+++ resources/views/layouts/view_child.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.view_plantilla')
 
 @section('title',view('layouts.view_title'))@show
+@section('css',view('layouts.view_css'))@show
 @section('menu',view('layouts.view_menu'))@show
+@section('header',view('layouts.view_header'))@show
+@section('footer',view('layouts.view_footer'))@show
+@section('opciones',view('layouts.view_opciones'))@show
+@section('js',view('layouts.view_js'))@show
---
+++ resources/views/layouts/view_css.blade.php
@@ -0,0 +1,7 @@
+<link rel="icon" type="image/png" href="{{ url('/') }}/public/img/logo_sentinelsin.png" />
+<link REL="SHORTCUT ICON" HREF="{{ url('/') }}/public/img/logo_sentinelsin.png" />
+<link href="{{ asset('public/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('public/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+<link href="{{ asset('public/css/plugins/morris/morris-0.4.3.min.css')}}" rel="stylesheet">
+<link href="{{ asset('public/css/animate.css') }}" rel="stylesheet">
+<link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
